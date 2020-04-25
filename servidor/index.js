04095be4e6b1d4c0c6b362378f31a5bd09d5d698
freilinjb <freilinjb@gmail.com>
2020-04-25 08:56:31 -0400

    probando usuario en log

--- servidor/index.js
+++ servidor/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 //Importar rutas
 
 //Definir la pagina principal
-app.get('/api/usuarios',require('./routes/usuarios'));
+app.use('/api/usuarios',require('./routes/usuarios'));
 
 //arrancando la app
 app.listen(PORT, () => {
--- servidor/routes/usuarios.js
+++ servidor/routes/usuarios.js
@@ -1,11 +1,13 @@
 //Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 //Crear un usuario
-//api/usuario
-
+//api/usuario 
+//Recibe un request de tipo post
 router.post('/', () => {
-
+    console.log('Crear usuario...');
+    
 });
 module.exports = router;
