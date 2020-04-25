a83af75af39029f95a3b23294884a3e7f85b0260
freilinjb <freilinjb@gmail.com>
2020-04-25 09:19:36 -0400

    registrando usuario en mongoose

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,3 +1,18 @@
-exports.crearUsuario = (req, res) => {
-    console.log(req.body);
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+    try {
+        let usuario;
+
+        usuario = new Usuario(req.body);
+
+        //guar el nuevo usuario
+        await usuario.save();
+
+        //Mensaje de confirmacion
+        res.send('Usuario creado correctamente');
+    } catch (error) {
+        console.log(error);
+        res.state(400).send('Hubo un error');
+    }
 }
