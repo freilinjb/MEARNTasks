d8cc585e4cf0eb3a6a9994f1ba15bf33970a60c8
freilinjb <freilinjb@gmail.com>
2020-04-25 15:43:45 -0400

    probando si el usuario No existe

--- servidor/controllers/authController.js
+++ servidor/controllers/authController.js
@@ -5,6 +5,7 @@ const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
 //Copiando de usuarioController
+//api/auth
 exports.autenticarUsuario = async (req, res) => {
         //Revisar si hay errores
         const errores = validationResult(req);
@@ -15,6 +16,22 @@ exports.autenticarUsuario = async (req, res) => {
         //Verificar cuando alguien intente iniciar sesion 
         //verificar que el usuario exista 
         //Comparar la clasve y luego generar el JWT
+
+        const {email, password } = req.body
+
+        try {
+            //Revisar que sea un usuario registrado
+            let usuario = await Usuario.findOne({email});
+            if(!usuario) {
+                return res.status(400).json({msg: 'El usuario no existe'});
+            }
+
+            //Revisar el password 
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
 
         
 }
