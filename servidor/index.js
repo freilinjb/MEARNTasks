4fc017436d8feef9424efbdb3722fc11124e7db5
freilinjb <freilinjb@gmail.com>
2020-04-24 20:52:55 -0400

    Arrancando el servidor

--- servidor/index.js
+++ servidor/index.js
@@ -2,3 +2,17 @@ const express = require('express');
 
 //crear el servidor
 const app = express();
+
+//Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Definir la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+//arrancando la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+    
+});
