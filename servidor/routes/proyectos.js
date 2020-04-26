f306cef42f7237fb819b51e2ef416cf3d4c9704c
freilinjb <freilinjb@gmail.com>
2020-04-26 13:34:22 -0400

    validacion de los proyectos

--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 //Crear un usuario
 //api/proyectos
 router.post('/', 
     auth,//Primero verifica el auth si se autentica pasa a la ejecucion
+    [
+        check('nombre','El nomber del proyecto es obligatorio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
 );
 
