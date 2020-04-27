544a6c28de7d2ed8de7431ae9197518d2b2d8d84
freilinjb <freilinjb@gmail.com>
2020-04-27 18:09:32 -0400

    creando validacion en el formulario nuecaCuenta = no probado

--- proyecto/src/components/auth/NuevaCuenta.jsx
+++ proyecto/src/components/auth/NuevaCuenta.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const NuevaCuenta = () => {
+
+    //extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
 
     //State para iniciar sesion
     const [usuario, setUsuario] = useState({
@@ -27,6 +32,9 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         //validar campos
+        if(nombre.trim() === '' || email.trim() === '' || password.trim() === '' ||confirmar.trim() === '') {
+            mostrarAlerta('Todos los campos son obligatorios','alerta-error');
+        }
 
         //Password minimo de 6 caracteres
 
@@ -36,6 +44,7 @@ const NuevaCuenta = () => {
     }
     return ( 
         <div className="form-usuario">
+            { alerta ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>)  : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener una cuenta</h1>
                 <form onSubmit={onSubmit}>
