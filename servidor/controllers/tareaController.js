481d101875f9f16177c6b594781dfce50807afd4
freilinjb <freilinjb@gmail.com>
2020-04-27 00:47:58 -0400

    agregado editar tareas v24 - 15

--- servidor/controllers/tareaController.js
+++ servidor/controllers/tareaController.js
@@ -67,3 +67,41 @@ exports.obtenerTareas = async (req, res) => {
         res.status(500).send('Hubo un error');
     }
 }
+
+exports.actualizarTarea = async (req, res) => {
+    try {
+        //Extraer el proyecto y comprobar si existe
+        const { proyecto, nombre, estado } = req.body
+
+        //Si la tarea existe o no
+        let tarea = await Tarea.findById(req.params.id);
+
+        if(!tarea) {
+            return res.status(404).json({msg: 'No existe esa tarea'});
+        }
+
+        //Extraer proyecto
+        const existeProyecto = await Proyecto.findById(proyecto);
+
+        //Revisar si el proyecto actual pertenece al usuario autenticado
+        //creadir esta en el modelo y en la BD
+        if(existeProyecto.creador.toString() !== req.usuario.id) { //Revisa de lo sesion del usuario si es el mismo
+            return res.status(401).json({msg: 'No Autorizado'});
+        }
+
+        //Crear un objeto con la nueva informacion
+        const nuevaTarea = {};
+
+        if(nombre) nuevaTarea.nombre = nombre;
+        if(estado) nuevaTarea.estado = estado;
+
+        //Guardar la tarea
+        tarea = await Tarea.findOneAndUpdate({_id: req.params.id}, nuevaTarea,{new: true});
+
+        res.json({tarea});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
